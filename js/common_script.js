d5c14c9f609f00e0c55cb24fc12a55621e58ffb7
sese98 <128338655+sese98@users.noreply.github.com>
2023-03-20 12:46:30 +0900

    Update common_script.js

--- js/common_script.js
+++ js/common_script.js
@@ -2,14 +2,6 @@ $(document).on('click', 'a[href="#"]', function (e) {
   e.preventDefault(); /* a 클릭할 때 위로 튕기는 이벤트의 속성을 제거하라 */
 });
 
-// topBtn
-// $(window).scroll(function(){
-// 	if ($(this).scrollTop() > 300){
-// 		$('.topBtn').show();
-// 	} else{
-// 		$('.topBtn').hide();
-// 	}
-// });
 
 $('.topBtn').click(function(){
 	$('html, body').animate({scrollTop:0},0);
@@ -39,22 +31,6 @@ $(function () {
 });
 
 
-//헤더 이벤트
-$(function() {
-  let prevScrollTop = 0;
-  document.addEventListener("scroll", function() {
-    let nowScrollTop = $(window).scrollTop(); 
-    if (nowScrollTop > prevScrollTop){ 
-      $('.topBtn').addClass('active'); 
-      $('header').addClass('active'); 
-    } else { 
-      $('.topBtn').removeClass('active'); 
-      $('header').removeClass('active'); 
-    } 
-    prevScrollTop = nowScrollTop; 
-  });
-});
-
 
 //메뉴 토글
 $(function() {
@@ -75,4 +51,4 @@ $(function() {
 
 // let path = document.querySelector('.skills .chart61percent path').getTotalLength();
 // console.log(path);
-// alert(path)
+
